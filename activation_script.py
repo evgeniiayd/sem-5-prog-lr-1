c2ae14432669f40a695e7a683a3b5849a07f20af
evgeniiayd <125044330+evgeniiayd@users.noreply.github.com>
2024-12-26 19:44:03 +0300

    Update activation_script.py

--- activation_script.py
+++ activation_script.py
@@ -2,18 +2,21 @@ import re
 import sys
 from urllib.request import urlopen
 
-import myremotemodule
+import requests
 
 
 def url_hook(some_str):
     if not some_str.startswith(("http", "https")):
         raise ImportError
-    with urlopen(some_str) as page:  # requests.get()
-        data = page.read().decode("utf-8")
-    filenames = re.findall("[a-zA-Z_][a-zA-Z0-9_]*.py", data)
-    modnames = {name[:-3] for name in filenames}
-    return URLFinder(some_str, modnames)
-
+    try:
+        data = requests.get(some_str).text
+        # data = page.read().decode("utf-8")
+        filenames = re.findall("[a-zA-Z_][a-zA-Z0-9_]*.py", data)
+        modnames = {name[:-3] for name in filenames}
+        return URLFinder(some_str, modnames)
+    except urllib.error.HTTPError as e:
+        print(f"Ошибка доступа к удаленному модулю: {e.reason}")
+        quit()
 
 sys.path_hooks.append(url_hook)
 print(sys.path_hooks)
@@ -44,6 +47,7 @@ class URLFinder(PathEntryFinder):
 # для "перехваченного" адреса URL модуля будем делать импорт
 
 from urllib.request import urlopen
+import urllib.error
 
 class URLLoader:
     def create_module(self, target):
@@ -55,5 +59,9 @@ class URLLoader:
         code = compile(source, module.__spec__.origin, mode="exec")
         exec(code, module.__dict__)
 
+sys.path_hooks.append(url_hook)
+sys.path.append('https://evgeniiayd.github.io/my-remote-module/')
+
+import myremotemodule
 
 myremotemodule.myfoo()
